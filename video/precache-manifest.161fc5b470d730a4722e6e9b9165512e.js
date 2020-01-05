1806f974fc7c6bf890cb3330bea2f1b3e27287f3
fb <demos_bin@163.com>
2020-01-05 13:04:26 +0800

    video

---
+++ video/precache-manifest.161fc5b470d730a4722e6e9b9165512e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05653296e6f35786a9f92f978973e117",
+    "url": "/index.html"
+  },
+  {
+    "revision": "757b9ebbc6b765635a87",
+    "url": "/static/css/2.2fb2da46.chunk.css"
+  },
+  {
+    "revision": "2e57b58cd1d8cae0f07b",
+    "url": "/static/css/main.7ba283b8.chunk.css"
+  },
+  {
+    "revision": "757b9ebbc6b765635a87",
+    "url": "/static/js/2.1be348e1.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.1be348e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e57b58cd1d8cae0f07b",
+    "url": "/static/js/main.0e88c8fb.chunk.js"
+  },
+  {
+    "revision": "835b6ff0828933af4f7c",
+    "url": "/static/js/runtime-main.5fbea10d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
